57762040ec04e657537d84488a83bd8d8950e2c1
Nujood AlMadhi <nujoodalmadi@gmail.com>
2019-07-17 17:27:27 +0300

    vendor pending

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const db = require("@arangodb").db;
+const _ = require("lodash");
 const createRouter = require("@arangodb/foxx/router");
 const router = createRouter();
 const joi = require("joi");
@@ -26,7 +27,7 @@ router
           },
           {
             timestamp: null,
-            status_received: false
+            status_received: false // true - false - failed
           },
           {
             timestamp: null,
@@ -46,13 +47,8 @@ router
 
       if (orderToStoreObj) {
         res.status(200);
-        console.log("I'm here");
         customerOrders.save(orderToStoreObj); // creates new doc
         res.send(orderToStoreObj.json);
-      } else {
-        res.status(200).send({
-          status: false
-        });
       }
     } catch (e) {
       catchE(res, e);
@@ -62,5 +58,55 @@ router
   .summary("Send order to store")
   .description("This endpoint sends an order to the store")
   .pathParam("customer_id", joi.string().required(), "customer id (key)");
+
+//Fetches checked-out orders that need to be "accepted" by the store
+
+router
+  .get("/vendor_pending", (req, res) => {
+    try {
+      const pendingOrders = customerOrders
+        .toArray()
+        .filter(
+          order =>
+            order.status[0].status_checkout === true &&
+            order.status[1].status_received === false
+        );
+      if (!pendingOrders || pendingOrders.length === 0) {
+        res.send("No pending orders");
+      }
+      res.json(
+        _.map(pendingOrders, order => {
+          return _.omit(order, ["driver_id", "status"]);
+        })
+      );
+    } catch (e) {
+      catchE(res, e);
+    }
+  })
+  .summary("fetches orders to be accepted by the store")
+  .description(
+    "This endpoint fetches orders from etest_customer_orders collection to be accepted by the store"
+  );
+
+//recieves order_id  from front-end, then updates the order (status_received: true)
+router
+  .patch("/status_recieved/:order_id", function(req, res) {
+    try {
+      let order = customerOrders.document(req.pathParams.order_id);
+      order.status[1] = {
+        timestamp: Date.now(),
+        status_received: true // true - false - failed
+      };
+      let updatedOrder = customerOrders.update(req.pathParams.order_id, order);
+      res.send(updatedOrder);
+    } catch (e) {
+      catchE(res, e);
+    }
+  })
+  .pathParam("order_id", joi.string().required(), "id of the order.")
+  .summary("changes the state of the order => recieved by store")
+  .description(
+    "changes the state of the order => recieved by changing the status in etest_customer_orders collection "
+  );
 
 module.exports = router;
