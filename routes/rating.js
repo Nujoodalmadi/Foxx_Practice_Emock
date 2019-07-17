f9ba453dcef5b13456b31789763cee1dfe34905d
Nujood AlMadhi <nujoodalmadi@gmail.com>
2019-07-17 12:10:42 +0300

    rating module fixed

--- routes/rating.js
+++ routes/rating.js
@@ -1,17 +1,20 @@
+"use strict";
 const createRouter = require("@arangodb/foxx/router");
 const router = createRouter();
+const joi = require("joi");
 
 //Collections
 const ratingColl = module.context.collection("customer_rating");
 
+router.tag("customer");
 router
   .post("/rating", function(req, res) {
     const data = req.body;
     const meta = ratingColl.save(req.body);
     res.send(Object.assign(data, meta));
   })
-  .body(joi.object().required(), "New shop to be stored")
-  .summary("Retrieve all documents")
-  .description("Retrieves all documents");
+  .body(joi.object().required(), "new rating")
+  .summary("posts a customer rating ")
+  .description("posts a customer rating to etest_customer_rating collection ");
 
 module.exports = router;
