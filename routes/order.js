a7d91efa337175a2d5da1602db7b299632a0c346
Nujood AlMadhi <nujoodalmadi@gmail.com>
2019-07-18 12:50:58 +0300

    order model import modified

--- routes/order.js
+++ routes/order.js
@@ -6,7 +6,7 @@ const createRouter = require("@arangodb/foxx/router");
 const router = createRouter();
 const joi = require("joi");
 const catchE = require("./error");
-const orderModel = "../models/order";
+const orderModel = require("../models/order");
 
 //COLLECTIONS
 const customerOrders = module.context.collection("customer_orders");
